6cbcc098e3158dce681f50b2882f39733b42fd47
Pieter Stragier <pieter@Pieters-iMac.local>
2018-04-27 21:20:52 +0200

    Initial Commit

---
+++ SplitTree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SplitTree
+//
+//  Created by Pieter Stragier on 27/04/2018.
+//  Copyright © 2018 PWS-apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
